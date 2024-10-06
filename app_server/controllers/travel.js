ff7aaded09397e03e57ecdb13c51661a2b3c2bc6
tmitch94 <tacia.mitchell@icloud.com>
2024-10-05 21:58:03 -0400

    update travel.js
    included a check if the json does not find anything or if json is not an array

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -11,12 +11,19 @@ const options = {
 
 // var fs = require('fs');
 // var trips = JSON.parse(fs.readFileSync(`./data/trips.json`, `utf-8`));
-
+let message = null;
 const travel = async function(req, res,next){
     await fetch(tripEndpoint, options)
     .then(res=> res.json())
     .then(json => {
-        console.log(json);
+        // console.log(json);
+        
+        if(!(json instanceof Array)){
+            message = 'API lookup error';
+            json=[];
+        }else if(!json.length){
+                message = 'No trips exist in our database';
+            }
         res.render('travel', {
             title: 'Travlr Getaways', 
             trips: json,
