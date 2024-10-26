0371e1f3be232bdcb25fd7691ff3f0e901ac2981
tmitch94 <tacia.mitchell@icloud.com>
2024-10-26 01:51:21 -0400

    update to contact

--- app_server/controllers/contact.js
+++ app_server/controllers/contact.js
@@ -1,7 +1,10 @@
-/*GET Homepage*/
+var fs = require('fs');
+var contacts = JSON.parse(fs.readFileSync(`data/contact.json`, `utf-8`));/*GET Homepage*/
+
 const contact = (req,res) => {
     res.render('contact', {
         title: "Travlr Getaways",
+        contacts,
         isContact: true
     });
 };
