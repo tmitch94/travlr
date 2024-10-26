b7e69c9c1fab5ca3f34f0e0e04ac09e86dfdd9a0
tmitch94 <tacia.mitchell@icloud.com>
2024-10-25 23:05:10 -0400

    adding hbs

---
+++ app_server/routes/index.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let router = express.Router();
+const ctrlMain = require ('../controllers/main')
+
+/* GET home page. */
+router.get('/', ctrlMain.index);
+
+module.exports = router;
